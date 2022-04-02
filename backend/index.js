17c0ce9f1d3ecd65106735b710913ccdd6c5172a
Hamzah Contreras <Hamzah_36@outlook.com>
2022-04-02 00:03:56 -0500

    Done

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,71 @@
 
 
 const webSocket = require('ws');
-const wss = new webSocket.Server({port: 7000})
-const clients = {};
+const wss = new webSocket.Server({port: 3000})
+
+const connections = {};
 const users = {};
+const chats = {};
+
+
+function getChat(a,b)
+{
+    const [x,y] = a < b ? [a, b] : [b, a]
+    if (!(x in chats)) chats[x] = {}
+    if (!(y in chats[x])) chats[x][y] = []
+    return chats[x][y]
+}
+
+
+function sendMessage(sender, receiver, message) {  
+    console.log("sender: " + sender + " receiver: " + receiver)
+    const chat = getChat(sender,receiver)
+    chat.push({message : message , sender : sender , receiver : receiver})
+    console.log(chat)
+}
+
+
 wss.on('connection', (ws) => {
+
     const id = uuidv4()
-    clients[id] = ws
+    connections[id] = {connection : ws}
 
     ws.on('message', (textMessageIn) => {
         const obj = JSON.parse(textMessageIn)
 
         console.log(obj)
 
-        const textMessageOut = JSON.stringify(obj)
-        for (const id in clients) {
-            clients[id].send(textMessageOut)
-        }    
+        if (obj.action === "login")
+        {
+            users[obj.name] = { name : obj.name , picture: obj.picture, connection : id }
+            connections[id].user = obj.name
+            updateUserList()
+        }
+        if (obj.action === "message")
+        {
+            if ("user" in connections[id])
+            {
+                console.log("before send")
+                sendMessage(connections[id].user, obj.receiver, obj.message)
+                updateUserList()
+            }
+        }
+
+        // const textMessageOut = JSON.stringify(obj)
+        // for (const id in clients) {
+        //     clients[id].send(textMessageOut)
+        // }    
     })
 
-    ws.on('onClose', () => {
-        delete clients[id]
-        delete users[id]
+    ws.on('close', () => {
+        if ("user" in connections[id]) // user was logged in but disconnected
+        {
+            delete users[connections[id].user]
+        }
+        delete connections[id]
+        updateUserList()
     })
-    users[id] = {name: id}
+
 })
 
 function uuidv4(){
@@ -34,4 +76,40 @@ function uuidv4(){
         return (c=='x' ? r :(r&0x3|0x8)).toString(16);
     });
     return uuid;
+}
+
+function updateUserList() {
+        for (const id in connections) {
+            sendUserList(id)
+        }
+}
+
+function sendUserList(id)
+{
+    if ("user" in connections[id])
+    {
+        const user = connections[id].user
+        let userList = {}
+        for (const u in users) if (u !== user) { 
+            const messages = getChat(user, users[u].name).map( m => {
+                    return { message : m.message , who : (user === m.sender ? "me" : "you")}
+            })
+            userList[u] = { name : users[u].name , picture: users[u].picture, messages : messages}
+        }
+        const textMessageOut = JSON.stringify(userList)
+        console.log(userList)
+        connections[id].connection.send(textMessageOut)
+    }
+    /*
+    else
+    {
+        const userList = {}
+        for (const u in users) {
+             userList[u] =  { name : users[u].name, picture: users[u].picture, messages : []}
+        }
+        const textMessageOut = JSON.stringify(userList)
+        console.log(userList)
+        connections[id].connection.send(textMessageOut)
+    }
+    */
 }
